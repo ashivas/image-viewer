71f5c8863d926bd086e0a71af4adb888b69b4203
ASHISH SHIVHARE <36054066+v-ashivhare@users.noreply.github.com>
2021-05-22 01:13:10 -0400

    Added Static Header to the app

---
+++ src/common/header/Header.js
@@ -0,0 +1,14 @@
+import './Header.css';
+import React from 'react';
+
+const Header = ()=>{
+    return(
+        <div>
+            <header className="app-header">
+                <div className="header-title">Image Viewer</div>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/screens/home/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Header from '../../common/header/Header'
+
+class Home extends Component {
+
+    render() {
+        return(
+            <Header/>
+        )
+    }
+}
+
+export default Home;
